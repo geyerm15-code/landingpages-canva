f5ed7818749165ea8f3939d39e4478e9afc8b283
Geyer Rivadeneira <geyerm15@gmail.com>
2026-07-28 12:20:47 -0500

    Limpiar duplicadas en elementor-generator

--- lib/elementor-generator.ts
+++ lib/elementor-generator.ts
@@ -100,8 +100,6 @@ function buildDeviceContainer(
     flex_justify_content: "flex-end",
     width: { unit: "%", size: "100" },
     height: device === "mobile" ? { unit: "px", size: "700" } : { unit: "px", size: "1120" },
-    width: { unit: "%", size: "100" },
-    height: device === "mobile" ? { unit: "px", size: "700" } : { unit: "px", size: "1120" },
     background_background: "classic",
     background_color: "#000000",
     padding: ZERO_SPACING,
